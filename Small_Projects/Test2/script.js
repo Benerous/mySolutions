f2df710ff37bb95d481918631c89e10123021255
Benerous <Benerous@users.noreply.github.com>
2020-01-17 11:50:06 +0200

    Added from local machine

---
+++ Small_Projects/Test2/script.js
@@ -0,0 +1,35 @@
+function function_0() {
+    document.getElementById("row_1").style.opacity = "1";
+    document.getElementById("row_1").style.position = "static";
+    document.getElementById("row_2").style.opacity = "1";
+    document.getElementById("row_2").style.position = "static";
+    document.getElementById("row_3").style.opacity = "1";
+    document.getElementById("row_3").style.position = "static";
+}
+
+function function_1() {
+    document.getElementById("row_1").style.opacity = "1";
+    document.getElementById("row_1").style.position = "static";
+    document.getElementById("row_2").style.opacity = "0";
+    document.getElementById("row_2").style.position = "fixed";
+    document.getElementById("row_3").style.opacity = "0";
+    document.getElementById("row_3").style.position = "fixed";
+}
+
+function function_2() {
+    document.getElementById("row_1").style.opacity = "0";
+    document.getElementById("row_1").style.position = "fixed";
+    document.getElementById("row_2").style.opacity = "1";
+    document.getElementById("row_2").style.position = "static";
+    document.getElementById("row_3").style.opacity = "0";
+    document.getElementById("row_3").style.position = "fixed";
+}
+
+function function_3() {
+    document.getElementById("row_1").style.opacity = "0";
+    document.getElementById("row_1").style.position = "fixed";
+    document.getElementById("row_2").style.opacity = "0";
+    document.getElementById("row_2").style.position = "fixed";
+    document.getElementById("row_3").style.opacity = "1";
+    document.getElementById("row_3").style.position = "static";
+}
